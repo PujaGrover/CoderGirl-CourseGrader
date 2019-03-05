2255d448229cdfa5b1980b18115ac52ce9a3b502
Puja Grover <puja_grover@yahoo.com>
2019-03-04 18:50:58 -0600

    Week6HwCourseGrader:commit,pus,sync. Issue: Test does not run. Code works well

--- CourseGrader/Program.cs
+++ CourseGrader/Program.cs
@@ -11,6 +11,41 @@ namespace CourseGrader
             // It should then return a message of "pass" or "fail" depending on these two conditions:
             // If the average score is greater than or equal to 70 and no single test score is below 50, then return a message of "pass".
             // If the average score is lower than 70 or at least one test score is below 50, then return a message of "fail".
+
+            int [] testScores = new int[] {70, 70, 55, 70, 70, 70};
+            Console.WriteLine(GradeTestScores(testScores));
+            Console.ReadLine();
+        }
+
+        private static string GradeTestScores(int[] testScores)
+        {
+            int sum = 0;
+            for (int i = 0; i < testScores.Length; i++)
+            {
+                if (testScores[i] < 50)
+                { 
+                    Console.WriteLine(testScores[i]);
+                    return "fail";                   
+                }
+                else
+                { 
+                sum += testScores[i];
+                Console.WriteLine(sum);
+                }
+            }
+
+            int averageScore = sum / testScores.Length;
+            Console.WriteLine(sum);
+            Console.WriteLine(averageScore);
+            if (averageScore < 70)
+            {
+                return "fail";
+            }
+            else
+            {
+                return "pass";
+            }
         }
     }
-}
+}   
+
